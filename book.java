24eacf9dd312006acea89eae2d5bb00b001ef8af
ymrm <s1511514@u.tsukuba.ac.jp>
2019-02-16 23:25:15 +0900

    基本3

--- book.java
+++ book.java
@@ -4,11 +4,20 @@ class Book{ //クラスBookの宣言
 	private int price;
 //	private float price_f;
 	private float yenprice;
+	private int year;
 
 	Book(String t, int p, float p2){
 	title = t;
 	price = p;
     yenprice = p2 * 120.47f;
+	}
+
+	public void setYear(int y){ //西暦の取得	
+		year = y;
+	}
+
+	public int getYear(){ //西暦の取得	
+		return year;
 	}
 
 	public void setPrice(int p){ //priceの書き換え
